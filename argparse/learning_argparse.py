e49ae8f737fbe07296f408a046829205f1817021
Shane Wang <chwang12341@gmail.com>
2020-06-20 14:45:53 +0800

    create Argparse Learning

---
+++ argparse/learning_argparse.py
@@ -0,0 +1,73 @@
+## learning_argparse.py
+
+
+## import package
+from argparse import ArgumentParser
+
+## Introduce
+
+##parameter
+## prog : program name
+## usage : how to use your program (default=None, it will replace with your program name)
+## description: describe about your program
+## epilog: additional materials
+parser1 = ArgumentParser(prog = "my argparse example", usage = None, 
+                         description = "learn how to use argparse",
+                         epilog = "https://github.com/chwang12341/Learn-Python-")
+# print(parser1)
+
+##ArgumentParser(prog='my argparse example', 
+##usage=None, 
+##description='learn how to use argparse', 
+##formatter_class=<class 'argparse.HelpFormatter'>, 
+##conflict_handler='error', 
+##add_help=True)
+
+
+# print(parser1.print_help())
+##usage: my argparse example [-h]
+
+##learn how to use argparse
+
+##optional arguments:
+##  -h, --help  show this help message and exit
+##
+##https://github.com/chwang12341/Learn-Python-
+##None
+
+## Example
+## import package
+## from argparse import ArgumentParser
+
+parser = ArgumentParser()
+## add static parameters (type=int, str) 
+parser.add_argument('runtimes', type=int, help='display an integer')
+parser.add_argument('name', type=str, help='display a string')
+
+## add Optional parameters
+## dest: after parsing, the variable name
+parser.add_argument('--place', type=str, help='display an integer', dest = 'position')
+parser.add_argument('--food', type=str, help='display an integer')
+
+args = parser.parse_args()
+## print variable
+# print (args.runtimes)
+# print (args.name)
+
+## function
+def count(runtimes,name):
+    for i in range(runtimes):
+        print(str(name)+' run ' + str(i) +' times' )
+
+
+##execute function
+if __name__ == "__main__":
+    ## Optional parameters
+    if  args.position:
+        print('Place: '+ args.position)
+    if args.food:
+        print('food: '+ args.food)
+    
+    ## static parameters
+    count(args.runtimes, args.name)
+    
---
+++ argparse/readme.md
@@ -0,0 +1,65 @@
+
+
+## Argparse
+
+**for controling variable through external **
+
+
+
+### Import Packages
+
+import argparse
+
+
+
+### Introduction 
+
+ArgumentParser()  parameter:
+
++ prog : program name
+
++ usage : how to use your program (default=None, it will replace with your program name)
+
++ description: describe about your program
+
++ epilog: additional materials
+
+![argparse1](C:\Users\user\Desktop\Learn-Python-\argparse\images\argparse1.PNG)
+
+
+
+### Example
+
+1. Optional parameters
+
+   + control variable (ex.args.runtimes)
+   + type: int 、str
+
+2. Additional parameters
+
+   you can choose to give the paremeter or not
+
+   + use "--" to form addtional parameters
+
+   + deset: after parsing, the variable name
+
+     ex. add parameter: parser.add_argument('--place', type=str, help='display an integer', dest = 'position')
+
+     ​      form variable name: args.position
+
+![argparse2](C:\Users\user\Desktop\Learn-Python-\argparse\images\argparse2.PNG)
+
+### Execute python
+
+go to cmd and try to run your python like this
+
+1. python learning_argparse.py 10 Shane                             : Static Parameter
+2. python learning_argparse.py 10 Shane --place forest     : Additional Parameter (**you can choose to give the paremeter or not**)
+3. python learning_argparse.py 10 Shane --food meat        : Additional Parameter
+4. python learning_argparse.py 10 Shane --place forest --food toast : Additional Parameter
+
+![argparse3](C:\Users\user\Desktop\Learn-Python-\argparse\images\argparse3.PNG)
+
+
+
+*reference: <https://wiki.jikexueyuan.com/project/explore-python/Standard-Modules/argparse.html>*
